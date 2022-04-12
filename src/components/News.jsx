da33cd5126e4192c28e28c7a6fcf892aa225e684
Saulo Freitas <69553799+SauloFreitas01@users.noreply.github.com>
2022-04-11 22:28:37 -0300

    fetching news from api with redux

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import{Select, Typography,Row,Col,Avatar,Card} from 'antd'
+import moment from 'moment'
 
-const News = () =>{
+import {useGetCryptoNewsQuery} from '../services/CryptoNewsApi'
+
+const{Text,Title} = Typography;
+const {Option} = Select
+
+const News = ({simplfied}) =>{
+    const{data: cryptoNews} = useGetCryptoNewsQuery({newsCategory:'Cryptocurrency',count: simplfied ? 10:100})
+    console.log(cryptoNews)
     return (
         <div>
                <span>News </span>
